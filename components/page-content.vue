2a273d155a78d84e9d0c6177f8cad87f3cc6f9e4
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-02-26 15:33:32 +0100

    small layout fix

--- components/page-content.vue
+++ components/page-content.vue
@@ -8,7 +8,7 @@
       <div v-if="element.type === 'text'" class="prose lg:prose-md" v-html="$md.render(element.markdown)"></div>
       <logos v-if="element.type === 'logos'" :logos="element.logo"/>
 
-      <div v-if="element.type === 'buttons'" class="flex flex-row flex-wrap w-full gap-2">
+      <div v-if="element.type === 'buttons'" class="flex flex-row flex-wrap w-full gap-2 my-4">
         <div v-for="(b, key) in element.button" :key="`button-${key}`">
           <a v-if="b.download" :href="b.download" download class="group hover:bg-pink-700  px-1 pb-1 md:pb md:px-2 rounded border-2 border-pink-500" style="text-decoration: none !important">
             <span class="prose text-gray-600 group-hover:text-gray-100">{{ b.label }}</span>
